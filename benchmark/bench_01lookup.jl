b3710d206f043675f9d31d6da249f0bfd4e1aec7
Andrey Oskin <skoffer@arkoniak.com>
2020-01-21 09:15:45 +0200

    basic lookup benchmark

--- benchmark/bench_01lookup.jl
+++ benchmark/bench_01lookup.jl
@@ -1,10 +1,9 @@
 module BenchLookup
 using BenchmarkTools
 using SymSpell
+using SymSpell: VerbosityALL
 
 suite = BenchmarkGroup()
-# d = Dictionary()
-# update!(d, joinpath(@__DIR__, "..", "assets", "frequency_dictionary_en_82_765.txt"))
 
 d = Dictionary()
 
@@ -13,6 +12,13 @@ push!(d, "bc", 2)
 s = "abc"
 
 suite["simple_lookup"] = @benchmarkable lookup($d, $s, max_edit_distance = 1)
+
+d2 = Dictionary(joinpath(@__DIR__, "..", "assets", "frequency_dictionary_en_82_765.txt"))
+s1 = "quintesestnial"
+s2 = "goophr"
+
+suite[s1] = @benchmarkable lookup($d2, $s1)
+suite[s2] = @benchmarkable lookup($d2, $s2, verbosity = VerbosityALL)
 
 end
 
